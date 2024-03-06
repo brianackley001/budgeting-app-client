39d19fb5edf157ff798a4db021b90bcdc3fe1426
brianackley001 <brian.ch.ackley@gmail.com>
2024-03-05 19:53:12 -0800

    Refactor AccountSummaryList unit tests

--- src/components/accounts/AccountSummaryList.spec.tsx
+++ src/components/accounts/AccountSummaryList.spec.tsx
@@ -1,29 +1,69 @@
 import { render, screen } from '@testing-library/react';
 import AccountSummaryList from "./AccountSummaryList";
-import AccountSectionAccordion  from "./AccountSectionAccordion.tsx";
+
+
+const sectionLabels: string[] = ["Checking & Savings", "Credit Cards", "Investments", "Loans"];
 
 beforeEach(() => {
-  let items = [
+  let accountItems = [
     {
-      account_id: "lv5KvMeBlgTmRMMjPgz9Sbr5QmDxKXcpzo5Dp",
+      accountId: "KzNKXRVLzbhvX6yd7Ke3t0KQx9eZAmUERokXz",
+      customName: "",
+      includeAccountTransactions: true,
+      institutionId: "ins_1234567",
+      institutionName: "Institution 1",
       balances: {
         available: 100,
         current: 110,
-        iso_currency_code: "USD",
+        isoCurrencyCode: "USD",
         limit: null,
-        unofficial_currency_code: null
+        unofficialCurrencyCode: null
       },
-      mask: "0000",
+      itemId: "Vz8PV3L6zNhdqLJn49xwhV3wZ8d9wPfrEnvgv",
+      mask: "3544",
       name: "Plaid Checking",
-      official_name: "Plaid Gold Standard 0% Interest Checking",
+      officialName: "Plaid Checking",
       subtype: "checking",
       type: "depository",
-      includeInTransactions: true,
+      dateCreated: "Wed, 28 Feb 2024 01:33:55 GMT",
+      dateUpdated: "Wed, 06 Mar 2024 03:18:20 GMT"
+    },
+    
+    {
+      accountId: "kqpoeV5Kqmsk7j091Xy8FLmkNMV7PRCo7egjd",
+      customName: "",
+      includeAccountTransactions: true,
+      institutionId: "ins_98979694",
+      institutionName: "Institution 2",
+      balances: {
+        available: 100,
+        current: 110,
+        isoCurrencyCode: "USD",
+        limit: null,
+        unofficialCurrencyCode: null
+      },
+      itemId: "Vz8PV3L6zNhdqLJn49xwhV3wZ8d9wPfrEnvgv",
+      mask: "9999",
+      name: "Plaid Credit Card",  
+      officialName: "Plaid Credit Card",
+      subtype: "credit card",
+      type: "credit",
+      dateCreated: "Wed, 28 Feb 2024 01:33:55 GMT",
+      dateUpdated: "Wed, 06 Mar 2024 03:18:20 GMT"
     }
   ];
-  let item = render(<AccountSummaryList items={items} />);
+  let item = render(<AccountSummaryList items={accountItems} />);
 });
-test('renders AccountSummaryList', () => {
-  const linkElement = screen.getByText(/Plaid Checking/i);
-  expect(linkElement).toBeInTheDocument();
+
+describe("AccountSummaryList", () => {
+  test('renders section headers', () => {
+    sectionLabels.forEach((sectionLabel) => {
+      expect(screen.getByText(sectionLabel)).toBeInTheDocument();
+    } );
+  });
+  test('renders expected account names', () => {
+    expect(screen.getByText(/Plaid Checking/i)).toBeInTheDocument();
+    expect(screen.getByText(/Plaid Credit Card/i)).toBeInTheDocument();
+  });
 });
+
