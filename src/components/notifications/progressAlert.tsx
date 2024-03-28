18a8f2fe7c8159eb22f7ee648486b8f2717242a7
brianackley001 <brian.ch.ackley@gmail.com>
2024-03-28 07:16:20 -0700

    Address issues with autohide timer - see: https://github.com/react-bootstrap/react-bootstrap/pull/5220
    resolves #82

--- src/components/notifications/progressAlert.tsx
+++ src/components/notifications/progressAlert.tsx
@@ -1,6 +1,5 @@
-import { useState } from 'react';
-import { Alert, AlertHeading, Container, Navbar, Spinner, Toast, ToastContainer   } from 'react-bootstrap';
-import divWithClassName from 'react-bootstrap/esm/divWithClassName';
+import { useState, useEffect } from "react";
+import { Container, Spinner, Toast, ToastContainer } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCheckCircle, faTriangleExclamation, faRotate } from '@fortawesome/free-solid-svg-icons'
 import { useAppSelector, useAppDispatch } from "@hooks/useStoreHooks";
@@ -8,54 +7,64 @@ import { selectHeaderText, selectIcon, selectInProgress, selectMessageText, sele
 
 
 function AlertDismissible() {
-  const showComponent = useAppSelector(selectShowAlert);
   const dispatch = useAppDispatch();
   const headerText = useAppSelector(selectHeaderText);
-  const messageText = useAppSelector(selectMessageText);
   const icon = useAppSelector(selectIcon);
   const inProgress = useAppSelector(selectInProgress);
+  const messageText = useAppSelector(selectMessageText);
+  const showComponent = useAppSelector(selectShowAlert);
   const variantStyle = useAppSelector(selectVariantStyle);
-  // const toastTimeout = variantStyle === "danger" ? 7000 :
-  //   variantStyle === "info" ? 65000 :
-  //     variantStyle === "success" ? 2500 :
-  //       4000;
+  const [autoHide, setAutoHide] = useState(true);
+  const [toastTimeout, setToastTimeout] = useState(1000);
 
+  useEffect(() => {
+    if (variantStyle === "error") {
+      setToastTimeout(7000);
+      setAutoHide(true);
+    } else if (variantStyle === "info") {
+      setToastTimeout(960000);
+      setAutoHide(false);
+    } else if (variantStyle === "success") {
+      setToastTimeout(2500);
+      setAutoHide(true);
+    } else {
+      setToastTimeout(4000);
+      setAutoHide(true);
+    }
+  }, [autoHide, variantStyle]);
   return (
-    <>
-      <Container fluid>
-        <ToastContainer
-          className="p-3"
-          position="middle-center"
-          style={{ zIndex: 10 }}>
-          <Toast onClose={() => { dispatch(setShowAlert(false)) }}
-            show={showComponent}
-            delay={variantStyle === "error" ? 7000 : 3000}
-            autohide
-            className="d-inline-block m-1 p-3"
-            style={{ zIndex: 1 }}
-            bg={variantStyle}>
-            <Toast.Header>
-              <strong className="me-auto">
-                {headerText}
-              </strong>
-            </Toast.Header>
-            <Toast.Body className={'text-white'}>
-              {inProgress ? <Spinner animation="border" role="status" size="sm" /> : null}
-              <p>
-                <span className='iconStyle'>
-                  {icon && icon.iconType === 'error' && icon.isVisible ? <FontAwesomeIcon icon={faTriangleExclamation} /> : null}
-                  {icon && icon.iconType === 'success' && icon.isVisible ? <FontAwesomeIcon icon={faCheckCircle} /> : null}
-                  {icon && icon.iconType === 'sync' && icon.isVisible ? <FontAwesomeIcon icon={faRotate} /> : null}
-                </span>
-                {messageText}
-              </p>
-            </Toast.Body>
-          </Toast>
-
-        </ToastContainer>
-      </Container>
-    </>
+    <Container fluid>
+      <ToastContainer
+        className="p-3"
+        position="middle-center"
+        style={{ zIndex: 10 }}>
+        <Toast onClose={() => { dispatch(setShowAlert(false)) }}
+          show={showComponent}
+          delay={toastTimeout}
+          autohide={autoHide}
+          className="d-inline-block m-1 p-3"
+          style={{ zIndex: 1 }}
+          bg={variantStyle}>
+          <Toast.Header>
+            <strong className="me-auto">
+              {headerText}
+            </strong>
+          </Toast.Header>
+          <Toast.Body className={'text-white'}>
+            {inProgress ? <Spinner animation="border" role="status" size="sm" /> : null}
+            <p>
+              <span className='iconStyle'>
+                {icon && icon.iconType === 'error' && icon.isVisible ? <FontAwesomeIcon icon={faTriangleExclamation} /> : null}
+                {icon && icon.iconType === 'success' && icon.isVisible ? <FontAwesomeIcon icon={faCheckCircle} /> : null}
+                {icon && icon.iconType === 'sync' && icon.isVisible ? <FontAwesomeIcon icon={faRotate} /> : null}
+              </span>
+              {messageText}
+            </p>
+          </Toast.Body>
+        </Toast>
+      </ToastContainer>
+    </Container>
   );
 }
 
-export default AlertDismissible;
+export default AlertDismissible; 
