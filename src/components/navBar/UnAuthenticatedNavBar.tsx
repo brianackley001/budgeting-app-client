77724247908da48048000f32a2e76a04ec9243d7
brianackley001 <brian.ch.ackley@gmail.com>
2024-04-04 08:29:43 -0700

    SonarCloud - Unnecessary imports should be removed typescript:S1128
    https://sonarcloud.io/project/issues?resolved=false&id=brianackley001_budgeting-app-client&open=AY2Oz_8QBxQFQu1DQaCv

--- src/components/navBar/UnAuthenticatedNavBar.tsx
+++ src/components/navBar/UnAuthenticatedNavBar.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Container from "react-bootstrap/Container";
 import Navbar from "react-bootstrap/Navbar";
-import Nav from "react-bootstrap/Nav";
 
 import { SignInButton } from "../buttons/SignInButton";
 
