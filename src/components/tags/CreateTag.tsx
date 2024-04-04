07892d4ca98115b281cf0aac07cea856dadf449a
brianackley001 <brian.ch.ackley@gmail.com>
2024-04-04 07:37:22 -0700

    Sonar Cloud -  typescript:S1082
    https://sonarcloud.io/project/issues?resolved=false&sinceLeakPeriod=true&id=brianackley001_budgeting-app-client&open=AY3tFO6k3BKAVbcZzmcn

--- src/components/tags/CreateTag.tsx
+++ src/components/tags/CreateTag.tsx
@@ -67,7 +67,7 @@ export default function CreateTag(props) {
 
   return (
     <>
-      <Button variant="outline-primary" size="sm" className='addAccountButton' onClick={toggleModal}>
+      <Button variant="outline-primary" size="sm" className='addAccountButton' onClick={toggleModal}  role="button">
         <FontAwesomeIcon icon={faPlus} className='iconStyle' /><span onClick={toggleModal}>Create Tag</span>
       </Button>
 
