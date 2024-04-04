839fbd479e43a450dab971325a92c87bbb7ef772
brianackley001 <brian.ch.ackley@gmail.com>
2024-04-04 11:40:15 -0700

    SonarCloud fixes

--- src/components/transactions/filterOptions/NotesAccordionItem.tsx
+++ src/components/transactions/filterOptions/NotesAccordionItem.tsx
@@ -3,19 +3,17 @@ import { Accordion, Form} from 'react-bootstrap';
 export default function NotesAccordionItem(props) {
   const { eventKey, onSelect, trackedValue } = props;
   return (
-    <>
-      <Accordion.Item eventKey={eventKey}>
-        <Accordion.Header>Notes</Accordion.Header>
-        <Accordion.Body>
-          <Form.Group className="mb-3" controlId="formUserNotes">
-            {/* <Form.Label>Notes</Form.Label> */}
-            <Form.Control as="textarea" rows={1} placeholder="Search for..." 
+    <Accordion.Item eventKey={eventKey}>
+      <Accordion.Header>Notes</Accordion.Header>
+      <Accordion.Body>
+        <Form.Group className="mb-3" controlId="formUserNotes">
+          {/* <Form.Label>Notes</Form.Label> */}
+          <Form.Control as="textarea" rows={1} placeholder="Search for..."
             onChange={(event) => { onSelect(event) }}
             value={trackedValue}
             data-testid="notes-accordion-item-text-search-text-value" />
-          </Form.Group>
-        </Accordion.Body>
-      </Accordion.Item>
-    </>
+        </Form.Group>
+      </Accordion.Body>
+    </Accordion.Item>
   )
 };
