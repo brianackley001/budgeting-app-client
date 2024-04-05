54710a13f0d90e00a3f122a6d438473727ee4a52
brianackley001 <brian.ch.ackley@gmail.com>
2024-04-05 08:27:26 -0700

    SonarCloud  ternary
    Extract this nested ternary operation into an independent statement
    
    https://sonarcloud.io/project/issues?open=AY2Oz_7nBxQFQu1DQaBh&id=brianackley001_budgeting-app-client

--- src/components/accounts/AccountSummaryList.tsx
+++ src/components/accounts/AccountSummaryList.tsx
@@ -1,5 +1,5 @@
-import AccountSectionAccordion  from "./AccountSectionAccordion.tsx";
-import { configuredSectionData } from "@utils/accountSectionConfig.ts"
+import AccountSectionAccordion from "./AccountSectionAccordion.tsx";
+import { configuredSectionData } from "@utils/accountSectionConfig.ts";
 
 /**
  * Renders List of Account Summary Items
@@ -8,30 +8,29 @@ import { configuredSectionData } from "@utils/accountSectionConfig.ts"
 
 
 function AccountSummaryList({ items }) {
-  const bankAccounts = items && items.length > 0 ? items.filter((item) => item.type === 'depository')
-    .sort((a, b) => a.balances.current < b.balances.current ? 1 : -1) : [];
-  const creditCards = items && items.length > 0 ? items.filter((item) => item.type === 'credit')
-    .sort((a, b) => a.balances.current < b.balances.current ? 1 : -1): [];
-  const loans = items && items.length > 0 ? items.filter((item) => item.type === 'loan')
-    .sort((a, b) => a.balances.current < b.balances.current ? 1 : -1): [];
-  const investmentAccounts = items && items.length > 0 ? items.filter((item) => item.type === 'investment')
-    .sort((a, b) => a.balances.current < b.balances.current ? 1 : -1): [];
+  const bankAccounts = items?.length > 0 ? items.filter((item) => item.type === 'depository')
+    .sort((a, b) => b.balances.current - a.balances.current) : [];
+  const creditCards = items?.length > 0 ? items.filter((item) => item.type === 'credit')
+    .sort((a, b) => b.balances.current - a.balances.current) : [];
+  const loans = items?.length > 0 ? items.filter((item) => item.type === 'loan')
+    .sort((a, b) => b.balances.current - a.balances.current) : [];
+  const investmentAccounts = items?.length > 0 ? items.filter((item) => item.type === 'investment')
+    .sort((a, b) => b.balances.current - a.balances.current) : [];
   const iconColorValue = "198754";
 
   const accountSections = configuredSectionData(bankAccounts, creditCards, investmentAccounts, loans, iconColorValue);
- 
 
   return (
     <>
-    {accountSections.map((section) => (
-      <AccountSectionAccordion items={section.items} 
-      balanceIsDebt={section.balanceIsDebt} 
-      testLabels={section.testLabels} 
-      iconColorValue={section.iconColorValue} 
-      iconId={section.iconId} 
-      sectionLabel={section.sectionLabel}
-      key={section.key} />
-    ))}
+      {accountSections.map((section) => (
+        <AccountSectionAccordion items={section.items}
+          balanceIsDebt={section.balanceIsDebt}
+          testLabels={section.testLabels}
+          iconColorValue={section.iconColorValue}
+          iconId={section.iconId}
+          sectionLabel={section.sectionLabel}
+          key={section.key} />
+      ))}
     </>
   );
 }
