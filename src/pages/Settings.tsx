503cc623ee765855649009e19a2f249f669bdc2d
brianackley001 <brian.ch.ackley@gmail.com>
2024-08-29 06:32:16 -0700

    Add csv file upload to Settings.tsx for POC

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -6,6 +6,7 @@ import EditTag from '@/components/tags/EditTag';
 import CreateTag from "@/components/tags/CreateTag";
 import { logEvent } from "@utils/logger";
 import  axiosInstance  from '@utils/axiosInstance';
+import { useState } from "react";
 
 
 export const Settings = () => {
@@ -14,6 +15,8 @@ export const Settings = () => {
   const tags = useAppSelector(state => state.userSlice.transactionTags);
   const userId = useAppSelector(state => state.userSlice.userId);
   const accounts = useAppSelector(state => state.accountSlice.accounts);
+  const [file, setFile] = useState();
+  const [array, setArray] = useState([]);
   const executeSyncAction = async() => { 
     console.log("executeSyncAction");
     logEvent("map-userId-to-transactions", { userId: userId, accounts: accounts.length.toString()});
@@ -29,6 +32,42 @@ export const Settings = () => {
     // validate item/institution error is resolved
     const response = await axiosInstance.post(`/transactionsUserSyncAll`, 
       {userId: userId});
+  };
+
+    const fileReader = new FileReader();
+
+    const handleOnCsvSelectChange = (e) => {
+        setFile(e.target.files[0]);
+    };
+    
+  const csvFileToArray = string => {
+    const csvHeader = string.slice(0, string.indexOf("\n")).split(",");
+    const csvRows = string.slice(string.indexOf("\n") + 1).split("\n");
+
+    const array = csvRows.map(i => {
+      const values = i.split(",");
+      const obj = csvHeader.reduce((object, header, index) => {
+        object[header] = values[index];
+        return object;
+      }, {});
+      return obj;
+    });
+
+    setArray(array);
+    console.log(array);
+  };
+
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+
+    if (file) {
+      fileReader.onload = function (event) {
+        const text = event.target?.result;
+        csvFileToArray(text);
+      };
+
+      fileReader.readAsText(file);
+    }
   };
   return (
     <div className="dashboardAccountContainer">
@@ -59,6 +98,30 @@ export const Settings = () => {
               {tags.map((tag, i) => (
                 <EditTag key={i} tag={tag} tags={tags} userId={userId} />
               ))}
+            </Card.Body>
+          </Card>
+          
+          <Card className="mb-5">
+            <Card.Subtitle className="mb-2 mt-2 mx-2 text-bold">
+            Import Historical Transaction data
+              <span className='cardHeaderIconRight' aria-label="Import Historical Transaction data" title="Import Historical Transaction data">
+              <form>
+                <input 
+                    type={"file"}
+                    id={"csvFileInput"}
+                    accept={".csv"}
+                    onChange={handleOnCsvSelectChange}/>
+                <Button variant="primary" 
+                  className="ml-auto"
+                  data-testid="button-import-csv"
+                  onClick={(e) => {
+                    handleOnSubmit(e);
+                }}>Import</Button>
+            </form>
+              </span>
+            </Card.Subtitle>
+            <Card.Body>
+              
             </Card.Body>
           </Card>
 
